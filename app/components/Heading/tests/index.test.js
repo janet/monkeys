d9f8386177152781610a1752a54832314ec64980
janet <jskenmotsu@gmail.com>
2016-12-08 14:05:56 -0800

    adding Heading tests

--- app/components/Heading/tests/index.test.js
+++ app/components/Heading/tests/index.test.js
@@ -1,11 +1,34 @@
-// import Heading from '../index';
+import Heading from '../index';
+import Button from 'components/Button';
 
 import expect from 'expect';
-// import { shallow } from 'enzyme';
-// import React from 'react';
+import { mount } from 'enzyme';
+import React from 'react';
 
 describe('<Heading />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  const item = 'Wed 5/20 5-6pm';
+  const icon = 'fa fa-play';
+  it('to render with an icon', () => {
+    const renderedComponent = mount(
+      <Heading
+        level={3}
+        item={item}
+        icon={icon}
+      />
+    );
+    expect(renderedComponent.text()).toEqual(item);
+    expect(renderedComponent.find(Button).length).toEqual(1);
+    expect(renderedComponent.find('h3').length).toEqual(1);
+  });
+  it('to render without an icon', () => {
+    const renderedComponent = mount(
+      <Heading
+        level={2}
+        item={item}
+      />
+    );
+    expect(renderedComponent.find(Button).length).toEqual(0);
+    expect(renderedComponent.text()).toEqual(item);
+    expect(renderedComponent.find('h2').length).toEqual(1);
   });
 });
