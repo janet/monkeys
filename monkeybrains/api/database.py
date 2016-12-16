c4b04aa85a35708f2fdf494b658174521bbfbd4a
janet <jskenmotsu@gmail.com>
2016-12-16 10:35:58 -0800

    delete commented out code

--- monkeybrains/api/database.py
+++ monkeybrains/api/database.py
@@ -7,13 +7,6 @@ db = SQLAlchemy()
 
 def connect_db(app):
     """Connects to the specific database."""
-    # rv = sqlite3.connect(app.config['DATABASE'])
-    # rv.row_factory = sqlite3.Row
-    # return rv
-
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/monkeys'
-    # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.app = app
     db.init_app(app)
 
--- monkeybrains/server.py
+++ monkeybrains/server.py
@@ -1,5 +1,3 @@
-# all the imports
-import os
 from flask import Flask
 
 
@@ -7,13 +5,6 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
-# Load default config and override config from an environment variable
-# app.config.update(dict(
-#     DATABASE=os.path.join(app.root_path, 'monkeys.db'),
-#     SECRET_KEY='development key',
-#     USERNAME='admin',
-#     PASSWORD='default'
-# ))
 
 # import from api package at the end of the file here to
 # circumvent circular import issues
