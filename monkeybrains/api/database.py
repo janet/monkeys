5f009c34b88cb13be57f8d0f1e48c6061e612e6d
janet <jskenmotsu@gmail.com>
2016-12-16 10:33:25 -0800

    created postgres database and connected app to it

--- monkeybrains/api/database.py
+++ monkeybrains/api/database.py
@@ -1,50 +1,25 @@
-import sqlite3
-from flask import g
-# from flask_sqlalchemy import SQLAlchemy
-
-from .server import app
-
-# db = SQLAlchemy()
+# import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 
-def connect_db():
+db = SQLAlchemy()
+
+
+def connect_db(app):
     """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+    # rv = sqlite3.connect(app.config['DATABASE'])
+    # rv.row_factory = sqlite3.Row
+    # return rv
 
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/monkeys'
     # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    # db.app = app
-    # db.init_app(app)
+    db.app = app
+    db.init_app(app)
 
 
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+if __name__ == "__main__":
+    from server import app
+    connect_db(app)
 
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    print('Connected to database.')
--- monkeybrains/server.py
+++ monkeybrains/server.py
@@ -1,21 +1,19 @@
 # all the imports
 import os
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from flask import Flask
 
 
-# create our little application :)
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 # Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'monkeys.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
+# app.config.update(dict(
+#     DATABASE=os.path.join(app.root_path, 'monkeys.db'),
+#     SECRET_KEY='development key',
+#     USERNAME='admin',
+#     PASSWORD='default'
+# ))
 
 # import from api package at the end of the file here to
 # circumvent circular import issues
