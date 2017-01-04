9aa719544a6511fc67760ba8ab22a1c562161d89
janet <jskenmotsu@gmail.com>
2017-01-04 15:15:55 -0800

    remove pdb

--- monkeybrains/api/Student/endpoint.py
+++ monkeybrains/api/Student/endpoint.py
@@ -5,5 +5,4 @@ from .model import Student
 @app.route('/')
 def show_entries():
     ringo = Student.query.filter(Student.name_first == 'Ringo').one()
-    import pdb; pdb.set_trace()
     return ringo.name_first
