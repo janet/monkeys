9ab7198fc96fa25d1b4bc41b69bb945f763692d7
janet <jskenmotsu@gmail.com>
2017-05-17 09:39:57 -0700

    adding LoginForm validation

--- app/components/LoginForm/index.js
+++ app/components/LoginForm/index.js
@@ -10,45 +10,52 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { reduxForm, Field } from 'redux-form/immutable';
 
 import CenteredWrapper from 'components/Wrappers/CenteredWrapper';
+import { validate } from './Validate';
+
+const renderInput = ({ hintText, floatingLabelText, type, input, meta: { touched, error } }) =>
+  <TextField
+    hintText={hintText}
+    floatingLabelText={floatingLabelText}
+    type={type}
+    errorText={touched && error ? error : ''}
+    {...input}
+  />;
+
+renderInput.propTypes = {
+  hintText: PropTypes.string,
+  floatingLabelText: PropTypes.string,
+  type: PropTypes.string,
+  input: PropTypes.object,
+  meta: PropTypes.object,
+};
 
 function LoginForm(props) {
   const {
     handleSubmit,
-    pristine,
-    submitting,
   } = props;
 
-  console.log('pristine', pristine);
   return (
     <CenteredWrapper>
-      <form onSubmit={handleSubmit}>
+      <form autoComplete="off" onSubmit={handleSubmit}>
         <div>
           <Field
             name="email"
-            component={(email) =>
-              <TextField
-                hintText="person@example.com"
-                floatingLabelText="Email"
-                {...email.input}
-              />
-            }
+            hintText="person@example.com"
+            floatingLabelText="Email"
+            component={renderInput}
           />
         </div>
         <div>
           <Field
             name="password"
-            component={(password) =>
-              <TextField
-                type="password"
-                hintText="password"
-                floatingLabelText="Password"
-                {...password.input}
-              />
-            }
+            type="password"
+            hintText="password"
+            floatingLabelText="Password"
+            component={renderInput}
           />
         </div>
         <div>
-          <RaisedButton type="submit" disabled={pristine || submitting} label="Log In" primary />
+          <RaisedButton type="submit" label="Log In" primary />
         </div>
       </form>
     </CenteredWrapper>
@@ -57,11 +64,10 @@ function LoginForm(props) {
 
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
-  submitting: PropTypes.bool.isRequired,
-  pristine: PropTypes.bool.isRequired,
 };
 
 
 export default reduxForm({
   form: 'LoginForm',
+  validate,
 })(LoginForm);
